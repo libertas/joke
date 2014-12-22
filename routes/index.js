83fb7f08478064a8f02bc03c15187b8944d51cce
Libertas <horizonvei@gmail.com>
2014-12-22 12:08:41 +0800

    admin added

--- routes/index.js
+++ routes/index.js
@@ -7,16 +7,22 @@ db.run("CREATE TABLE IF NOT EXISTS joke (Title, Content)");
 /* GET home page. */
 router.get('/', function(req, res) {
   body = '<p>These are my passages:</p>';
-  db.all('SELECT * FROM joke', function(err, data){
-    data.forEach(function (datum){
+  db.all('SELECT * FROM joke', function(err, data) {
+    data.forEach(function (datum) {
       body = body + "<h3>" + datum.Title + "</h3>\n<p>" + datum.Content + "</p>\n";
     });
     res.render('index', { title: 'My Blog', content: body});
   });
 });
 
-router.get('/admin', function(req, res){
-  res.render('admin', { });
+router.get('/admin', function(req, res) {
+  query = req.query;
+  if (query.Submit == 'Submit'){
+	title = query.Title;
+	content = query.Content;
+	db.run("INSERT INTO joke VALUES (\"" + title + "\",\"" + content + "\")");
+  }
+  res.sendfile('./views/admin.html');
 });
 
 module.exports = router;
