ddfb823cd7f5ceaf31ed88250ec4997d95fe9715
Libertas <horizonvei@gmail.com>
2014-12-22 11:34:30 +0800

    bug of index page fixed

--- routes/index.js
+++ routes/index.js
@@ -6,12 +6,13 @@ db.run("CREATE TABLE IF NOT EXISTS joke (Title, Content)");
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  body = 'These are my passages:';
-  db.all("SELECT * from joke", function(err, data){
+  body = '<p>These are my passages:</p>';
+  db.all('SELECT * FROM joke', function(err, data){
     data.forEach(function (datum){
-    body = body + datum.Title + '\n\n' + datum.Content + '\n';
-    });});
-  res.render('index', { title: 'My Blog', content: body});
+      body = body + "<h3>" + datum.Title + "</h3>\n<p>" + datum.Content + "</p>\n";
+    });
+    res.render('index', { title: 'My Blog', content: body});
+  });
 });
 
 router.get('/admin', function(req, res){
